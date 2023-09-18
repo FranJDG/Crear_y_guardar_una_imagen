9063adfd6d77b31f0508d6c9b3dbd82738261657
Fran Diaz <frandiazguitar@gmail.com>
2023-09-18 17:35:48 +0200

    Agregar archivos de proyecto.

---
+++ Crear_y_guardar_una_imagen/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crear_y_guardar_una_imagen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = 200;  // Ancho de la imagen
+            int height = 200; // Alto de la imagen
+            string name = "Fran Díaz"; // Tu nombre
+
+            // Crear un nuevo objeto Bitmap con el ancho y alto especificados
+            using (Bitmap bitmap = new Bitmap(width, height))
+            {
+                // Crear un objeto Graphics a partir del bitmap
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    // Definir un fondo de color
+                    graphics.Clear(Color.Black);
+
+                    // Definir un color de texto
+                    Color textColor = Color.FromArgb(255, 255, 0, 0); // Rojo
+
+                    // Crear una fuente y un pincel para el texto
+                    using (Font font = new Font("Arial", 24))
+                    using (SolidBrush brush = new SolidBrush(textColor))
+                    {
+                        // Dibujar el nombre en el centro de la imagen
+                        SizeF textSize = graphics.MeasureString(name, font);
+                        PointF textLocation = new PointF((width - textSize.Width) / 2, (height - textSize.Height) / 2);
+                        graphics.DrawString(name, font, brush, textLocation);
+                    }
+                }
+
+                // Guardar la imagen generada
+                bitmap.Save("perfil_friki.png");
+            }
+
+            Console.WriteLine("Imagen de perfil generada con éxito. ¡Revísala en tu directorio de trabajo!");
+        }
+    }
+}
